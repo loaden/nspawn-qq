45d69114d025272e8a40eefff7b11315095d25f9
Yuchen Deng <loaden@gmail.com>
2021-09-23 22:47:14 +0800

    修复错误引号

--- debian-config.sh
+++ debian-config.sh
@@ -2,7 +2,7 @@
 # 维护：Yuchen Deng QQ群：19346666、111601117
 
 # 确认管理员权限
-if [ $UID != 0 -o ”$SUDO_USER“ == "root" ]; then
+if [ $UID != 0 -o "$SUDO_USER" == "root" ]; then
     echo "请先打开终端，执行 sudo -s 获得管理员权限后再运行本脚本。"
     exit 1
 fi

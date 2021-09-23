503fe53db3bcb547c8b63bbad16500d359450a46
Yuchen Deng <loaden@gmail.com>
2021-09-23 21:06:12 +0800

    支持Arch系一键安装配置

--- nspawn-deepin.sh
+++ nspawn-deepin.sh
@@ -10,6 +10,7 @@ fi
 
 # 创建容器
 [ -f /bin/apt ] && apt install -y systemd-container debootstrap
+[ -f /bin/pacman ] && pacman -Sy --noconfirm debootstrap
 mkdir -p /home/$SUDO_USER/.machines/deepin
 ln -sf /home/$SUDO_USER/.machines/deepin /var/lib/machines
 ln -sf /usr/share/debootstrap/scripts/stable /usr/share/debootstrap/scripts/apricot

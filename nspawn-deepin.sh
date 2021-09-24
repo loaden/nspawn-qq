5d2eb52faba88e172ca94c961c1236219dacae1b
Yuchen Deng <loaden@gmail.com>
2021-09-24 18:31:51 +0800

    Arch不更新包管理数据

--- nspawn-deepin.sh
+++ nspawn-deepin.sh
@@ -10,7 +10,7 @@ fi
 
 # 创建容器
 [ -f /bin/apt ] && apt install -y systemd-container debootstrap
-[ -f /bin/pacman ] && pacman -Sy --noconfirm debootstrap
+[ -f /bin/pacman ] && pacman -S --noconfirm debootstrap
 mkdir -p /home/$SUDO_USER/.machines/deepin
 ln -sf /home/$SUDO_USER/.machines/deepin /var/lib/machines
 ln -sf /usr/share/debootstrap/scripts/stable /usr/share/debootstrap/scripts/apricot

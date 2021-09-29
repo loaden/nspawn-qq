956a023a1365d1e40bec245d2a52a75dbc05da16
Yuchen Deng <loaden@gmail.com>
2021-09-29 21:51:50 +0800

    解决打开文件夹

--- deepin-config.sh
+++ deepin-config.sh
@@ -246,7 +246,7 @@ machinectl bind --read-only --mkdir deepin \$HOME/.local/share/fonts /home/u\$UI
 $(echo "$X11_BIND_AND_CONFIG")
 
 # 启动环境变量
-RUN_ENVIRONMENT="LANG=\$LANG DISPLAY=\$DISPLAY GTK_IM_MODULE=\$GTK_IM_MODULE XMODIFIERS=\$XMODIFIERS QT_IM_MODULE=\$QT_IM_MODULE"
+RUN_ENVIRONMENT="LANG=\$LANG DISPLAY=\$DISPLAY GTK_IM_MODULE=\$GTK_IM_MODULE XMODIFIERS=\$XMODIFIERS QT_IM_MODULE=\$QT_IM_MODULE BROWSER=Thunar"
 if [[ \$(loginctl show-session \$(loginctl | grep \$USER |awk '{print \$1}') -p Type) == *wayland* ]]; then
     RUN_ENVIRONMENT="\$RUN_ENVIRONMENT XAUTHORITY=\$XAUTHORITY"
 fi

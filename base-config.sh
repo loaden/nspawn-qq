5ea208bb923b890a9956471e15cf09f6b1b62e5c
Yuchen Deng <loaden@gmail.com>
2021-10-10 16:48:17 +0800

    修复脚本目录不存在

--- base-config.sh
+++ base-config.sh
@@ -20,6 +20,7 @@ fi
 # 初始化配置
 EXEC_FROM_CONFIG=1 source `dirname ${BASH_SOURCE[0]}`/remove-$1.sh
 ln -sf /home/$SUDO_USER/.machines/$1 /var/lib/machines/$1
+[ ! -d /usr/local/bin ] && mkdir /usr/local/bin
 
 
 # 允许无管理员权限启动

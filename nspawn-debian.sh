389ecaeacbfe4d97d3342f9d7cc1a9509d8b4453
Yuchen Deng <loaden@gmail.com>
2021-10-17 22:45:38 +0800

    完善执行脚本用户权限判断

--- nspawn-debian.sh
+++ nspawn-debian.sh
@@ -2,7 +2,7 @@
 # 维护：Yuchen Deng QQ群：19346666、111601117
 
 # 确认管理员权限
-if [ $UID != 0 -o ”$SUDO_USER“ == "root" ]; then
+if [[ $EUID != 0 ]]; then
     echo "请打开终端，在脚本前添加 sudo 执行，或者 sudo -s 获得管理员权限后再执行。"
     exit 1
 fi

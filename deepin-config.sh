9187a76f22bfc8123a7df6818a6e58a5402ac4d4
Yuchen Deng <loaden@gmail.com>
2021-11-14 22:21:36 +0800

    确保配置时update只执行一次

--- deepin-config.sh
+++ deepin-config.sh
@@ -21,10 +21,10 @@ SOURCES_LIST="echo 'deb [by-hash=force] https://community-packages.deepin.com/de
 echo 'deb https://com-store-packages.uniontech.com/appstore deepin appstore' > /etc/apt/sources.list.d/appstore.list"
 
 IFS='' read -r -d '' INSTALL_QQ <<EOF
-machinectl shell deepin /bin/bash -c "dpkg --add-architecture i386 && apt update && apt install -y com.qq.im.deepin x11-utils --no-install-recommends && apt autopurge -y"
+machinectl shell deepin /bin/bash -c "apt install -y com.qq.im.deepin x11-utils --no-install-recommends && apt autopurge -y"
 EOF
 IFS='' read -r -d '' INSTALL_WEIXIN <<EOF
-machinectl shell deepin /bin/bash -c "dpkg --add-architecture i386 && apt update && apt install -y com.qq.weixin.deepin x11-utils --no-install-recommends && apt autopurge -y"
+machinectl shell deepin /bin/bash -c "apt install -y com.qq.weixin.deepin x11-utils --no-install-recommends && apt autopurge -y"
 EOF
 
 

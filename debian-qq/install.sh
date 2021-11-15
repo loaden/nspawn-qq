4ad77ab13fadef0b7cff487d41e671b4af57b096
Yuchen Deng <loaden@gmail.com>
2021-11-15 17:59:28 +0800

    改进安装脚本

--- debian-qq/install.sh
+++ debian-qq/install.sh
@@ -7,7 +7,15 @@ if [ ! -f /usr/bin/zstd ]; then
     [ -f /usr/bin/dnf ] && /usr/bin/dnf install -y zstd
 fi
 
-sudo tar -xpvf `dirname ${BASH_SOURCE[0]}`/debian.tar.zst --directory=$HOME
+if [ ! -d $HOME/.machines ]; then
+    mkdir $HOME/.machines
+fi
+
+if [ -d $HOME/.machines/debian ]; then
+    echo 容器 debian 已经存在，安装之前必须先卸载并删除旧容器！ && sleep 2
+    source `dirname ${BASH_SOURCE[0]}`/remove.sh
+fi
+
+sudo tar -xpvf `dirname ${BASH_SOURCE[0]}`/debian.tar.zst --directory=$HOME/.machines
 sudo `dirname ${BASH_SOURCE[0]}`/nspawn-deepinwine/debian-config.sh
 echo 安装完成！
-
--- deepin-qq/remove.sh
+++ deepin-qq/remove.sh
@@ -7,7 +7,6 @@ echo -n -e "\033[31m需要我自动帮您删除~/.machines/deepin吗？[y/N]\033
 read -p " " choice
 case $choice in
 Y | y) sudo rm -rf $HOME/.machines/deepin && echo $HOME/.machines/deepin 已被删除！;;
-N | n | '') echo 再见！;;
-*) echo 错误选择，请手动删除！;;
+N | n | '') echo 再见！ && sleep 2 && exit ;;
+*) echo 错误选择，请手动删除！ && sleep 2 && exit 1 ;;
 esac
-

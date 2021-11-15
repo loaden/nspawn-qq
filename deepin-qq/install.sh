013c4c1ea19c478e871abba670aecef5ea73c837
Yuchen Deng <loaden@gmail.com>
2021-11-15 17:15:23 +0800

    Add install scripts

---
+++ deepin-qq/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# 维护：Yuchen Deng [Zz] QQ群：19346666、111601117
+
+if [ ! -f /usr/bin/zstd ]; then
+    [ -f /usr/bin/apt ] && /usr/bin/apt install -y zstd
+    [ -f /usr/bin/pacman ] && /usr/bin/pacman -S zstd
+    [ -f /usr/bin/dnf ] && /usr/bin/dnf install -y zstd
+fi
+
+sudo tar -xpvf `dirname ${BASH_SOURCE[0]}`/deepin.tar.zst --directory=$HOME
+sudo `dirname ${BASH_SOURCE[0]}`/nspawn-deepinwine/deepin-config.sh
+echo 安装完成！
+

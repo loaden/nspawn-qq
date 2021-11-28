2aea00b00deb090b780bb6b0fe8015a5b65ae2e9
Yuchen Deng <loaden@gmail.com>
2021-11-28 13:51:43 +0800

    修复安装脚本

--- deepin-qq/install.sh
+++ deepin-qq/install.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 # 维护：Yuchen Deng [Zz] QQ群：19346666、111601117
 
+# 仅允许普通用户权限执行
+if [ $EUID == 0 ]; then
+    echo $(basename $0) 命令只允许普通用户执行
+    exit 1
+fi
+
 if [ ! -f /usr/bin/zstd ]; then
-    [ -f /usr/bin/apt ] && /usr/bin/apt install -y zstd
-    [ -f /usr/bin/pacman ] && /usr/bin/pacman -S zstd
-    [ -f /usr/bin/dnf ] && /usr/bin/dnf install -y zstd
+    [ -f /usr/bin/apt ] && sudo /usr/bin/apt install -y zstd
+    [ -f /usr/bin/pacman ] && sudo /usr/bin/pacman -S zstd
+    [ -f /usr/bin/dnf ] && sudo /usr/bin/dnf install -y zstd
 fi
 
 if [ ! -d $HOME/.machines ]; then
